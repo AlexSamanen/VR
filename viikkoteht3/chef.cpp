691d998e7302de3187ef88901fd1640b281bbadc
AlexSamanen <samanen.alex@gmail.com>
2025-02-24 19:45:40 +0200

    chef.cpp

--- viikkoteht3/chef.cpp
+++ viikkoteht3/chef.cpp
@@ -1,29 +1,42 @@
+#include <iostream>
 #include "chef.h"
 
-Chef::Chef() {}
+using namespace std;
 
-Chef::Chef(string name)
+Chef::Chef(string n)
 {
-    chefName = name;
-    cout << "Chef defaultkonstruktori, ei nimeä"
-         << chefName
+    chefName = n;
+    cout << "Chef " << chefName << " konstruktori"
          << endl;
 }
 
 Chef::~Chef()
 {
-    cout <<"Chef destuktori"
+    cout << "Chef " << chefName << " destruktori"
          << endl;
 }
 
-string Chef::getChefName() const
+int Chef::makeSalad(int items)
+{
+    int portions = items / 5;
+    cout << "Chef " << chefName << " with " << items
+         << " items can make salad " << portions
+         << " portions"
+         << endl;
+    return portions;
+}
+
+int Chef::makeSoup(int items)
+{
+    int portions = items / 3;
+    cout << "Chef " << chefName << " with " << items
+         << " items can make soup " << portions
+         << " portions"
+         << endl;
+    return portions;
+}
+
+string Chef::getName()
 {
     return chefName;
-}
-
-int Chef::makeSalad(int aines)
-{
-    cout << "Aineksia "
-         << aines
-         << endl;
 }

e1c1559e02b1194113c9367082c3d3d9240fd508
AlexSamanen <samanen.alex@gmail.com>
2025-02-24 19:42:51 +0200

    game.cpp

--- game.cpp
+++ game.cpp
@@ -1,31 +1,53 @@
 #include "game.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-game::game(int mn)
+Game::Game(int maxN)
 {
-    maxNumber = mn;
-    cout << "game konstruktori"
-        << "maxNumber on "
-        <<maxNumber
-         << endl;
+    maxNumber = maxN;
+    playerQuess = 0;
+    numOfGuesses = 0;
+    srand(std::time(0)); 
+    cout<<"GAME CONSTRUCTOR: object initialized with "<< maxN<<" as a maximum value"<<endl;
 }
 
-game::~game()
+Game::~Game()
 {
-    cout << "Game destruktori"
-         << endl;
+    cout<<"GAME DESTRUCTOR: object cleared from stack memory"<<endl;
 }
 
-void game::play()
+void Game::play()
 {
-    cout << "game play"
-         << endl;
-    numOfGuesses = 7;
+    bool stayInLoop = true;
+    short randomNumber = (rand()%maxNumber)+1; 
+    numOfGuesses = 0;
+
+    while(stayInLoop)
+    {
+        cout<<"Give your guess between 1-"<<maxNumber<<endl;
+        cin>>playerQuess;
+        if(playerQuess == randomNumber)
+        {
+            cout<<"Your quess is right = "<<playerQuess<<endl;
+            stayInLoop = false;
+        }
+        else if(playerQuess<randomNumber)
+        {
+            cout<<"Your guess is too small"<<endl;
+        }
+        else
+        {
+            cout<<"Your guess is too big"<<endl;
+        }
+
+        numOfGuesses++;
+    }
     printGameResult();
-
 }
 
-void game::printGameResult()
+void Game::printGameResult()
 {
-    cout << "game printGame result"
-         << endl;
+    cout<<"You quessed the right answer = "<<playerQuess<<" with "<<numOfGuesses<<" quesses"<<endl;
 }

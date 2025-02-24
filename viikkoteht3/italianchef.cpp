0a2706ca5a5173c359d3da9064743d5a33d0c833
AlexSamanen <samanen.alex@gmail.com>
2025-02-24 19:46:52 +0200

    italianchef.cpp

--- viikkoteht3/italianchef.cpp
+++ viikkoteht3/italianchef.cpp
@@ -1,32 +1,39 @@
 #include "italianchef.h"
 
-ItalianChef::ItalianChef()
+ItalianChef::ItalianChef(string n)
+    : Chef(n)
 {
-    cout << "italianChef defaultkonstruktori"
-         << endl;
-}
-
-ItalianChef::ItalianChef(string name)
-    : Chef(name)
-{
-    cout << "italianChef konstruktori"
-        << ", kokin nimi "
-        << name
-         << endl;
+    cout << "ItalianChef " << chefName << " konstruktori" << endl;
 }
 
 ItalianChef::~ItalianChef()
 {
-    cout << "italianChef desktruktori"
-         << endl;
+    cout << "ItalianChef " << chefName << " destruktori" << endl;
 }
 
-bool ItalianChef::askSecret(string pw,int f, int w)
+bool ItalianChef::askSecret(string pw, int f, int w)
 {
-
+    if (password.compare(pw) == 0) {
+        cout << "Password ok for secret food!" << endl;
+        flour = f;
+        water = w;
+        makePizza();
+        return true;
+    }
+    cout << "Wrong password!" << endl;
+    return false;
 }
 
-int ItalianChef::MakePizza()
+int ItalianChef::makePizza()
 {
+    int portions_f = flour / 5;
+    int portions_w = water / 5;
+    int pizzas = min(portions_f,portions_w);
 
+    cout << "ItalianChef " << chefName << " with "
+         << flour << " flour and " << water
+         << " water can make " << pizzas
+         << " pizzas" << endl;
+
+    return pizzas;
 }

3f24ab0b2aa0e3c9d003226dfbc1e8de08df85f7
AlexSamanen <samanen.alex@gmail.com>
2025-02-24 19:38:48 +0200

    notifikaattori.h

--- viikko_teht5/notifikaattori.h
+++ viikko_teht5/notifikaattori.h
@@ -1,16 +1,18 @@
 #ifndef NOTIFIKAATTORI_H
 #define NOTIFIKAATTORI_H
 
+#include <iostream>
 #include "seuraaja.h"
 
 class Notifikaattori
 {
 public:
     Notifikaattori();
-    void lisaa(Seuraaja *);
-    void poista(Seuraaja *);
+
+    void lisaa(Seuraaja *s);
+    void poista(Seuraaja *s);
     void tulosta();
-    void postita(string v);
+    void postita(string viesti);
 
 private:
     Seuraaja *seuraajat = nullptr;

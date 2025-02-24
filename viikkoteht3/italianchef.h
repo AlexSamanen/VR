9cf562ec37c2cfd6fdb72be1c2dc8a98c27bea46
AlexSamanen <samanen.alex@gmail.com>
2025-02-24 19:47:26 +0200

    italianchef.h

--- viikkoteht3/italianchef.h
+++ viikkoteht3/italianchef.h
@@ -1,19 +1,21 @@
 #ifndef ITALIANCHEF_H
 #define ITALIANCHEF_H
-
 #include "chef.h"
 
 class ItalianChef : public Chef
 {
 public:
-    ItalianChef();
-    ItalianChef(string name);
+    ItalianChef(string n);
     ~ItalianChef();
 
     bool askSecret(string pw, int f, int w);
+
 private:
-    int MakePizza();
-    string password;// = "pizza";
+    int makePizza();
+
+    int flour;
+    int water;
+    const string password = "pizza";
 };
 
 #endif // ITALIANCHEF_H

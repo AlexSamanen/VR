e271627037991e273601d037ec4f4127dbba0477
AlexSamanen <samanen.alex@gmail.com>
2025-02-24 19:46:15 +0200

    chef.h

--- viikkoteht3/chef.h
+++ viikkoteht3/chef.h
@@ -1,6 +1,5 @@
 #ifndef CHEF_H
 #define CHEF_H
-
 #include <iostream>
 
 using namespace std;
@@ -8,13 +7,12 @@ using namespace std;
 class Chef
 {
 public:
-    Chef();
-    Chef(string name);
+    Chef(string n);
     ~Chef();
 
-    string getChefName() const;
-    int makeSalad(int aines);
-    int makeSoup(int aines);
+    string getName();
+    int makeSalad(int);
+    int makeSoup(int);
 
 protected:
     string chefName;

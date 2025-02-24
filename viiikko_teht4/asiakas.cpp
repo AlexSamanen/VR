8653801ae203cdff95b3c1f42d7de7112fe85c47
AlexSamanen <samanen.alex@gmail.com>
2025-02-24 19:28:40 +0200

    asiakas.cpp

--- viiikko_teht4/asiakas.cpp
+++ viiikko_teht4/asiakas.cpp
@@ -1,34 +1,75 @@
 #include "asiakas.h"
 
-
-
-Asiakas::Asiakas(string n, double lr)
-    : kayttotili(nimi), luottotili(nimi, lr)
+Asiakas::Asiakas(string n, double lr) :
+    kayttotili(n), luottotili(n,lr)
 {
-    nimi=n;
-    cout << "nimi="
-         << nimi;
-}
-
-bool Asiakas::talletus(double summa)
-{
-    cout << "asiakas "
-         << nimi
-         << " tallettaa"
-    bool onnistuiko = käyttötili.deposit(summa);
-    if (onnistuiko == false){
-        cout << "meni pieleen"
-             << endl;
-    }
-    return onnistuiko;
-}
-
-bool Asiakas::tilisiirto(double summa, Asiakas &saaja)
-{
-    bool onnistuiko = nosto();
+    nimi = n;
+    cout << "Asiakkuus luotu " << nimi << endl;
 }
 
 string Asiakas::getNimi() const
 {
     return nimi;
+}
+
+void Asiakas::showSaldo() const
+{
+    cout << "Kayttotilin saldo " << kayttotili.getBalance() << endl;
+    cout << "Luottotilin saldo " << luottotili.getBalance() << endl;
+}
+
+bool Asiakas::luotonMaksu(double amount) {
+    // Negat. summa ei sallittu
+    if (amount < 0.0) {
+        cout << "Luoton maksu" << amount << " ei sallittu" << endl;
+        return false;
+    } else {
+        luottotili.deposit(amount);
+        return true;
+    }
+}
+
+bool Asiakas::luotonNosto(double amount) {
+    // Negat. summa ei sallittu
+    if (amount < 0.0) {
+        cout << "Luoton nosto " << amount << " ei sallittu" << endl;
+        return false;
+    } else {
+        luottotili.withdraw(amount);
+        return true;
+    }
+}
+
+bool Asiakas::tiliSiirto(double amount, Asiakas &saaja)
+{
+    cout << "Pankkitili: " << getNimi() << " siirtaa "
+         << amount << " " << saaja.getNimi() << ":lle" << endl;
+    cout << getNimi() << " ";
+    if (true == nosto(amount)) {
+        cout << saaja.getNimi() << " ";
+        saaja.talletus(amount);
+    }
+}
+
+bool Asiakas::nosto(double amount)
+{
+    // Negat. summa ei sallittu
+    if (amount < 0.0) {
+        cout << "Nosto " << amount << " ei sallittu" << endl;
+        return false;
+    } else {
+        kayttotili.withdraw(amount);
+        return true;
+    }
+}
+
+bool Asiakas::talletus(double amount)
+{
+    if (amount <= 0.0) {
+        cout << "Talletus " << amount << " ei sallittu" << endl;
+        return false;
+    } else {
+        kayttotili.deposit(amount);
+        return true;
+    }
 }
